5e54201c5c0a927d54190e3c8788b39f657a9045
Michaël L <michaellaurac@gmail.com>
2022-06-27 09:20:56 +0200

    Created an intial Dog class which muss not be changed and added two decorator functions Cute and Trained with their respective testing

--- dog.js
+++ dog.js
@@ -1,5 +1,23 @@
 class Dog {
+  constructor () {
+    this.cost = 50;
+  }
 
+  displayPrice () {
+    return `The dog costs $${this.cost}.`;
+  }
 };
 
-module.exports = Dog;
+function Cute (dog) {
+  const cuteDog = Object.create(dog);
+  cuteDog.cost = dog.cost + 20;
+  return cuteDog;
+}
+
+function Trained (dog) {
+  const trainedDog = Object.create(dog);
+  trainedDog.cost = dog.cost + 60;
+  return trainedDog;
+}
+
+module.exports = { Dog, Cute, Trained };
--- dog_test.js
+++ dog_test.js
@@ -6,7 +6,7 @@ const fileName = () => {
 /* eslint-env mocha */
 
 const wish = require("wish");
-const Dog = require("./dog.js");
+const { Dog, Cute, Trained } = require("./dog.js");
 
 describe("tests run on the 'dog.js' file:", () => {
   // setup test
@@ -14,5 +14,13 @@ describe("tests run on the 'dog.js' file:", () => {
     wish(fileName() === "dog_test.js");
   });
   // functional tests
-  console.log(Dog);
+  it("verifies that the cost of a dog is $50", () => {
+    wish((new Dog()).displayPrice() === "The dog costs $50.");
+  });
+  it("verifies that the cost of a cute dog is $70", () => {
+    wish(Cute(new Dog()).displayPrice() === "The dog costs $70.");
+  });
+  it("verifies that the cost of a trained cute dog is $130", () => {
+    wish(Trained(Cute(new Dog())).displayPrice() === "The dog costs $130.");
+  });
 });
